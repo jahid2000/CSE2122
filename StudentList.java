319088909e6de279513d4aee33c70da84d069887
jahid2000 <jahidulislam24814@gmail.com>
2021-12-06 11:59:30 +0600

    task1complete

--- StudentList.java
+++ StudentList.java
@@ -8,85 +8,85 @@ public class StudentList {
 //		Check arguments
         if (args[0].equals("a")) {
             System.out.println("Loading data ...");
-            try {           //try statement use to error handling
-            	//variable name  improvement
-                BufferedReader bufferreader = new BufferedReader(    //new bufferfile decelere
+            try {
+
+                BufferedReader s = new BufferedReader(
                         new InputStreamReader(
                                 new FileInputStream("students.txt")));
-                String st = bufferreader.readLine();//string name r is improvement
-                String i[] = st.split(",");
+                String r = s.readLine();//string name r is improvement
+                String i[] = r.split(",");
                 for (String j : i) {
                     System.out.println(j);
                 }
-            } catch (Exception e)
-
+            } catch (Exception e) {
+			}
             System.out.println("Data Loaded.");
-            //another statement passing
+
         } else if (args[0].equals("r")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader bufferreader = new BufferedReader(
+                BufferedReader s = new BufferedReader(
                         new InputStreamReader(
                                 new FileInputStream("students.txt")));
-                String st = bufferreader.readLine();
-                System.out.println(st); //string st is passing to see output
+                String r = s.readLine();
+                System.out.println(r);
                 String i[] = st.split(",");
                 Random x = new Random();
                 int y = x.nextInt();
                 System.out.println(i[y]);
-            } catch (Exception e)
-
+            } catch (Exception e) {
+			}
             System.out.println("Data Loaded.");
-            //go to another statement
+
         } else if (args[0].contains("+")) {
             System.out.println("Loading data ...");
             try {
-            	//variable s change to new variable
-                BufferedWriter bufferwriter = new BufferedWriter(
+
+                BufferedWriter r = new BufferedWriter(
                         new FileWriter("students.txt", true));
                 String t = args[0].substring(1);
                 Date d = new Date();
                 String df = "dd/mm/yyyy-hh:mm:ss a";
                 DateFormat dateFormat = new SimpleDateFormat(df);
                 String fd = dateFormat.format(d);
-                bufferwriter.write(", " + t + "\nList last updated on " + fd);//new variable passing
-                bufferwriter.close();//new variable passing
-            } catch (Exception e)
-
+                r.write(", " + t + "\nList last updated on " + fd);
+                r.close();
+            } catch (Exception e) {
+			}
 
             System.out.println("Data Loaded.");
-            //go to another else if statement
+
         } else if (args[0].contains("?")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader bufferreader = new BufferedReader(
+                BufferedReader s = new BufferedReader(
                         new InputStreamReader(
                                 new FileInputStream("students.txt")));
-                String st = bufferreader.readLine(); //variable name improvement into st
-                String i[] = st.split(",");
+                String r = s.readLine();
+                String i[] = r.split(",");
                 boolean done = false;
                 String t = args[0].substring(1);
-                for (int index = 0; index < i.length && !done; index++) {  //initialision index zero decelere
+                for (int index = 0; index < i.length && !done; index++) {
                     if (i[index].equals(t)) {
                         System.out.println("We found it!");
-                        break;  //done is improvement into break statement
+                        done=true;
                     }
                 }
-            } catch (Exception e)
-
+            } catch (Exception e) {
+			}
             System.out.println("Data Loaded.");
-            //go to another else if statement
+
         } else if (args[0].contains("c")) {
             System.out.println("Loading data ...");
             try {
-                BufferedReader bufferreader = new BufferedReader(
+                BufferedReader s = new BufferedReader(
                         new InputStreamReader(
                                 new FileInputStream("students.txt")));
-                String D = bufferreader.readLine();
-                char a[] = D.toCharArray(); //new variable deceleration charater type
-                boolean in_word = false;  //initialize boolean data type
-                int count = 0; //initially  zero deceleration
-                for (char c : a) {     //working loop statement
+                String D = s.readLine();
+                char a[] = D.toCharArray();
+                boolean in_word = false;
+                int count = 0;
+                for (char c : a) {
                     if (c == ' ') {
                         if (!in_word) {
                             count++;
@@ -97,11 +97,11 @@ public class StudentList {
                     }
                 }
                 System.out.println(count + " word(s) found " + a.length);
-            } catch (Exception e)
-
-            System.out.println("Data Loaded."); //print the data
+            } catch (Exception e) {
+			}
+            System.out.println("Data Loaded.");
         }
-        //System.out.println("Invalid number");
-    }
 
+    }
 }
+
